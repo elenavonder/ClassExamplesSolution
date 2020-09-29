aa930d86bc33ac77ab976d80c712b87cf4c7366d
Elena Vonder Meulen <evonder13@gmail.com>
2020-09-29 11:29:21 -0400

    Final Commit

--- ClassExamplesProject/Class.cs
+++ ClassExamplesProject/Class.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+//When they are gray, it means you aren't using them yet
 namespace ClassExamplesProject
 {
     class Class
     {
-        public int Id { get; set; }
+        private static int NextId = 1;//this is a field
+
+        public int Id { get; private set; }//this is a property
         public string Subject {get; set;}
         public int Section { get; set; }
-        public int InstructorId { get; set; }
+        public int? InstructorId { get; set; }
 
-        public Class(int Id, string subject, int section, int instructorId)
+        public void print()
         {
-            this.Id = Id;
+            Console.WriteLine($"Class: Id:{this.Id}, Subject: {this.Subject}, " +
+                $"Section: {this.Section}, InstructorId: {this.InstructorId}");
+        }
+
+        public Class(string subject, int section, int? instructorId)
+        {
+            this.Id = NextId++;//this makes each one unique
             this.Subject = subject;
             this.Section = section;
             this.InstructorId = instructorId;
+        }
+        public Class()
+        {
+            //good practice to always have a default constructor
+            //default constructor: a contructor that takes no parameters
         }
     }
 }
--- ClassExamplesProject/Program.cs
+++ ClassExamplesProject/Program.cs
@@ -6,6 +6,11 @@ namespace ClassExamplesProject
     {
         static void Main(string[] args)
         {
+            var cls1 = new Class("English", 101, null);
+            Console.WriteLine($"{cls1.Id}, {cls1.Subject}");
+
+            var cls2 = new Class("Basket Weaving", 901, null);
+            Console.WriteLine($"{cls2.Id}, {cls2.Subject}");
 
             var business = new Major();
             business.Id = 1;
